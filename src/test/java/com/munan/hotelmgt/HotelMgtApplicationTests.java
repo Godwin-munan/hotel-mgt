c5318240a0880f253bc83dfd9e3c6d005c715c80
Godwin-munan <godwin.jethro@forthcm.com>
2024-12-10 21:48:26 +0100

    Test implementation V1

--- src/test/java/com/munan/hotelmgt/HotelMgtApplicationTests.java
+++ src/test/java/com/munan/hotelmgt/HotelMgtApplicationTests.java
@@ -1,5 +1,6 @@
 package com.munan.hotelmgt;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,8 @@ class HotelMgtApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@BeforeEach
+	void init(){}
 
 }
