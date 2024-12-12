19f814db190760b012e883ded5605d24c58cf51d
Godwin-munan <godwinjethro@gmail.com>
2024-12-12 11:32:33 +0100

    Add custom servers to swagger ui docs

--- src/main/java/com/munan/hotelmgt/HotelMgtApplication.java
+++ src/main/java/com/munan/hotelmgt/HotelMgtApplication.java
@@ -1,6 +1,9 @@
 package com.munan.hotelmgt;
 
 import com.munan.hotelmgt.config.RsaKeyProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +11,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger Demo", version = "1.0", description = "Documentation APIs v1.0"),
+		servers = {@Server(url = "http://localhost:8080", description = "Local Server URL")})
 public class HotelMgtApplication {
 
 	public static void main(String[] args) {
