70835f90a1a447faaa03e4b0ccc5ac5a52cc27ff
Godwin-munan <godwinjethro@gmail.com>
2024-12-13 18:22:22 +0100

    Injected Enviromment bean to main class

--- src/main/java/com/munan/hotelmgt/HotelMgtApplication.java
+++ src/main/java/com/munan/hotelmgt/HotelMgtApplication.java
@@ -4,9 +4,12 @@ import com.munan.hotelmgt.config.RsaKeyProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.Environment;
 
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
@@ -14,6 +17,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @OpenAPIDefinition(info = @Info(title = "Swagger Demo", version = "1.0", description = "Documentation APIs v1.0"),
 		servers = {@Server(url = "http://localhost:8080", description = "Local Server URL")})
 public class HotelMgtApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(HotelMgtApplication.class);
+
+	private final Environment env;
+
+	public HotelMgtApplication(Environment env) {
+		this.env = env;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelMgtApplication.class, args);
